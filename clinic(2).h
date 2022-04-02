6a08dff7ae036326fd900a846a4fb84a9b586303
Coffeemate73 <102769743+Coffeemate73@users.noreply.github.com>
2022-04-02 13:59:16 -0400

    Update clinic(2).h

--- clinic(2).h
+++ clinic(2).h
@@ -1,1 +1,118 @@
+#ifndef CLINIC_H
+#define CLINIC_H
 
+//////////////////////////////////////
+// Module macro's (usable by any file that includes this header)
+//////////////////////////////////////
+
+// Display formatting options (Provided to student)
+// !!! DO NOT MODIFY THESE MACRO'S !!!
+#define FMT_FORM 1
+#define FMT_TABLE 2
+
+// C Strings: array sizes
+#define NAME_LEN 15
+#define PHONE_DESC_LEN 4
+#define PHONE_LEN 10
+
+//////////////////////////////////////
+// Structures
+//////////////////////////////////////
+
+// Data type: Phone
+// ToDo:
+
+struct Phone
+{
+   char description[PHONE_DESC_LEN+1];
+   char number[PHONE_LEN+1];
+};
+
+// Data type: Patient
+// ToDo:
+
+struct Patient
+{
+   int patientNumber;
+   char name[NAME_LEN+1];
+   struct Phone phone;
+};
+
+// ClinicData type: Provided to student
+// !!! DO NOT MODIFY THIS DATA TYPE !!!
+struct ClinicData
+{
+   struct Patient* patients;
+   int maxPatient;
+};
+
+//////////////////////////////////////
+// DISPLAY FUNCTIONS
+//////////////////////////////////////
+
+// Display's the patient table header (table format)
+void displayPatientTableHeader(void);
+
+// Displays a single patient record in FMT_FORM | FMT_TABLE format
+void displayPatientData(const struct Patient* patient, int fmt);
+
+//////////////////////////////////////
+// MENU & ITEM SELECTION FUNCTIONS
+//////////////////////////////////////
+
+// Menu: Main
+void menuMain(struct ClinicData* data);
+
+// Menu: Patient Management
+void menuPatient(struct Patient patient[], int max);
+
+// Menu: Patient edit
+void menuPatientEdit(struct Patient* patient);
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// !!! ALL the below functions need defining       !!!
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// !!! DO NOT MODIFY THE BELOW PROTOTYPES !!!
+
+// Display's all patient data in the FMT_FORM | FMT_TABLE format
+void displayAllPatients(const struct Patient patient[], int max, int fmt);
+
+// Search for a patient record based on patient number or phone number
+void searchPatientData(const struct Patient patient[], int max);
+
+// Add a new patient record to the patient array
+void addPatient(struct Patient patient[], int max);
+
+// Edit a patient record from the patient array
+void editPatient(struct Patient patient[], int max);
+
+// Remove a patient record from the patient array
+void removePatient(struct Patient patient[], int max);
+
+//////////////////////////////////////
+// UTILITY FUNCTIONS
+//////////////////////////////////////
+
+// Search and display patient record by patient number (form)
+void searchPatientByPatientNumber(const struct Patient patient[], int max);
+
+// Search and display patient records by phone number (tabular)
+void searchPatientByPhoneNumber(const struct Patient patient[], int max);
+
+// Get the next highest patient number
+int nextPatientNumber(const struct Patient patient[], int max);
+
+// Find the patient array index by patient number (returns -1 if not found)
+int findPatientIndexByPatientNum(int patientNumber, const struct Patient patient[], int max);
+
+//////////////////////////////////////
+// USER INPUT FUNCTIONS
+//////////////////////////////////////
+
+// Get user input for a new patient record
+void inputPatient(struct Patient* patient);
+// Get user input for phone contact information
+void inputPhoneData(struct Phone* phone);
+
+#endif // !CLINIC_H
